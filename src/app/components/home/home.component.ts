5ef80e8caaf4ccfdcc10fa21b3f747e7a33d01c6
ravelomananash <ravelomananash@gmail.com>
2022-09-12 15:57:51 +0300

    page home avec base db

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,12 +8,30 @@ import { CarteService } from 'src/app/services/carte.service';
 })
 export class HomeComponent implements OnInit {
   carts: any;
-
+  carteAuCinema: any;
+  carteAUCinema: any[] = [];
+  carteMemesis: any[] = [];
+  carteAllie: any[] = [];
   constructor(private api: CarteService) {}
 
   ngOnInit(): void {
     this.api.getCartes().subscribe((res) => {
       this.carts = res;
     });
+    for (let i = 1; i < 4; i++) {
+      this.api.getCarteById(i).subscribe((res) => {
+        this.carteAUCinema.push(res);
+      });
+    }
+    for (let i = 4; i < 7; i++) {
+      this.api.getCarteById(i).subscribe((res) => {
+        this.carteMemesis.push(res);
+      });
+    }
+    for (let i = 7; i < 10; i++) {
+      this.api.getCarteById(i).subscribe((res) => {
+        this.carteAllie.push(res);
+      });
+    }
   }
 }
